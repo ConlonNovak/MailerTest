3d591a85ed8c4396b89aeed177894837dbf3c681
Winston Chu <wpchu@andrew.cmu.edu>
2018-01-25 11:17:13 -0500

    set up email part

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,17 @@
+class UserMailer < ApplicationMailer
+
+  default from: "cmuplayerreadiness@gmail.com"
+
+  def pre_practice_email(user)
+    @user = user
+    @testURL = 'http://www.profh.net:8080'
+    mail(to: @user.email, subject: "Pre-Practice Survey Reminder")
+  end
+
+  def post_practice_email(user)
+    @user = user
+    @testURL = 'http://www.profh.net:8080'
+    mail(to: @user.email, subject: "Post-Practice Survey Reminder")
+  end
+
+end
