2be6f4df3bdef149771406c604c93eb6a8b0d3c9
Winston Chu <wpchu@andrew.cmu.edu>
2018-01-25 11:33:15 -0500

    figured out how applicationMailer works

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,8 +26,10 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.perform_deliveries = true
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -51,4 +53,13 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    user_name:            'cmuplayerreadiness@gmail.com',
+    password:             'wcalatqapla',
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 end
