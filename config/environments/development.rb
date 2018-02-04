61bd65cdfb9b1b77f287c65fa0434198a312aeb1
Winston Chu <wpchu@andrew.cmu.edu>
2018-02-04 12:00:04 -0500

    uses environmental variables for gmail username and pw now

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -58,8 +58,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    user_name:            'cmuplayerreadiness@gmail.com',
-    password:             'wcalatqapla',
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["GMAIL_PASSWORD"],
     authentication:       'plain',
     enable_starttls_auto: true  }
 end
