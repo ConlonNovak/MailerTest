6b07dfcf0489134b7e9cad4661de01afc7e130c5
Alec Lam <alechunterlam@gmail.com>
2018-02-07 16:23:14 -0500

    team ids

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,24 @@
 class User < ApplicationRecord
 
+  # Daily Wellness Survey @ 10 AM everyday
   def self.email_all
     User.all.each do |u|
-
-      if (u.id % 2 == 1)
-        UserMailer.post_practice_email(u).deliver_now
-      end
-      if (u.id % 2 == 0)
-        UserMailer.pre_practice_email(u).deliver_now
-      end
+      UserMailer.pre_practice_email(u).deliver_now
       #UserMailer.post_practice_email(u).deliver_now
     end
   end
+
+
+  # Men's Basketball 5
+
+  # Women's Basketball 6
+
+
+  # Women's Tennis 9
+
+
+  # Men's Tennis 8
+
 
   def self.testMethod
     puts User.all
