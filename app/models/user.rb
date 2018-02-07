4e813686199fc3358a47e1567d59c969ec7d7d24
Winston Chu <wpchu@andrew.cmu.edu>
2018-02-07 16:24:45 -0500

    methods for emailing specific teams post practice

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,15 +10,40 @@ class User < ApplicationRecord
 
 
   # Men's Basketball 5
+  def self.email_mbb
+    User.all.each do |u|
+      if u.team_id == 5
+        UserMailer.post_practice_email(u).deliver_now
+      end
+    end
+  end
 
   # Women's Basketball 6
-
+  def self.email_wbb
+    User.all.each do |u|
+      if u.team_id == 6
+        UserMailer.post_practice_email(u).deliver_now
+      end
+    end
+  end
 
   # Women's Tennis 9
-
+  def self.email_wtennis
+    User.all.each do |u|
+      if u.team_id == 9
+        UserMailer.post_practice_email(u).deliver_now
+      end
+    end
+  end
 
   # Men's Tennis 8
-
+  def self.email_mtennis
+    User.all.each do |u|
+      if u.team_id == 8
+        UserMailer.post_practice_email(u).deliver_now
+      end
+    end
+  end
 
   def self.testMethod
     puts User.all
