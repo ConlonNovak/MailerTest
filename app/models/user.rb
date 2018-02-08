74eecef197914413ce7281b620ba964d276fecfc
Winston Chu <wpchu@andrew.cmu.edu>
2018-02-08 17:17:33 -0500

    daily wellness email to everyone @ 10am

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ApplicationRecord
   def self.email_all
     User.all.each do |u|
       UserMailer.pre_practice_email(u).deliver_now
-      #UserMailer.post_practice_email(u).deliver_now
     end
   end
 
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,30 +1,13 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
-
-# Example:
-#
 set :output, "/Users/aleclamusr/Desktop/67-373/S18/MailerTest/log/cron.log"
 set :environment, 'development'
 
-#
-# every 3.minutes do
-#   runner "User.email_all"
-# end
 
-job_type :qapla, "cd /Users/aleclamusr/Desktop/67-373/S18/MailerTest && rvm use 2.4.1 && :task :second && rails runner 'User.email_all' "
+job_type :all, "cd /Users/aleclamusr/Desktop/67-373/S18/MailerTest && rvm use 2.4.1 && :task :second && rails runner 'User.email_all' "
+job_type :mbb, "cd /Users/aleclamusr/Desktop/67-373/S18/MailerTest && rvm use 2.4.1 && :task :second && rails runner 'User.email_mbb' "
+job_type :wbb, "cd /Users/aleclamusr/Desktop/67-373/S18/MailerTest && rvm use 2.4.1 && :task :second && rails runner 'User.email_wbb' "
+job_type :mtennis, "cd /Users/aleclamusr/Desktop/67-373/S18/MailerTest && rvm use 2.4.1 && :task :second && rails runner 'User.email_mtennis' "
+job_type :wtennis, "cd /Users/aleclamusr/Desktop/67-373/S18/MailerTest && rvm use 2.4.1 && :task :second && rails runner 'User.email_wtennis' "
 
-# every 3.minutes do
-#   runner 'User.email_all'
-# end
-
-every 1.minute do
-  qapla 'bundle', second: 'install'
+every 1.day, at: '10:00 am' do
+  all 'bundle', second: 'install'
 end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
-
-# Learn more: http://github.com/javan/whenever
